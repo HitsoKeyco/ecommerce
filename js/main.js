5962590254d471e2c44ebff2be9bcb2f23d6969b
Sergio Olivo <sergio_oliver009@hotmail.com>
2023-05-04 18:44:08 -0500

    version 0.007

--- js/main.js
+++ js/main.js
@@ -1,8 +1,29 @@
 //* Menu hamburguesa
-{
+
     const menu = document.querySelector(".menu");
     const links = document.querySelectorAll(".menu li");
     const boxMenu = document.querySelector(".menu_hamburguesa");
+    const navbarHMTL = document.querySelector(".nav")
+
+
+// -------------------------------------------------------
+//* Nav Transparente
+
+
+function animationScroll() {
+    let y = window.scrollY;
+
+    if (y > 200) {        
+        navbarHMTL.classList.add("nav_show");
+    } else {
+        
+        navbarHMTL.classList.remove("nav_show");
+    }
+}
+
+window.onscroll = () => animationScroll();
+
+// -------------------------------------------------------
 
     function mostrarMenu() {
         menu.classList.toggle("menu_show");
@@ -13,10 +34,13 @@
     links.forEach((link) => {
     link.addEventListener("click", mostrarMenu);
     });
-}
+
+
+    
+
 
 //* Conexion a API - Guardado datos en localStorage -  Peticion de datos al localStorage
-{
+
   // Promesa asincrona
     async function getProducts() {
         try {
@@ -41,54 +65,131 @@
         }
     }
 
+
+
   //* codigo accionador "arranca"para q espere nuestra promesa + guardar informacion en el localStorage
+
     async function main() {
         const db = {
             products:
-            JSON.parse(window.localStorage.getItem("products")) ||
-            (await getProducts()),
-            cart: {},
+                    JSON.parse(window.localStorage.getItem('products')) || (await getProducts()),
+            cart:   
+                    JSON.parse(window.localStorage.getItem('cart')) || {},
         };
 
-        const productsHTML = document.querySelector(".products");
+        pintarProducts(db)
+        mostrarCart();
+        insertarProductosAlCart(db)
+        validarDatosCart(db)
+    }
 
-        let html = "";
+    main();
 
-        for (product of db.products) {
+function pintarProducts(db){
+    const productsHTML = document.querySelector(".products");
 
-            html += ` 
-                    
-                    <div class="card">
+    let html = "";
 
-                        <div class="card_top">
-                            <img class="img_producto" src="${product.image}" alt="">
-                        </div>
+    for (product of db.products) {
 
-                        <div class="card_dawn">
-                            <span>$${product.price}&nbsp;&nbsp;&nbsp;</span>
-                            <span> Stock: ${product.quantity}</span><br>
-                            <p class = "description_product">${product.name}</p>
-                            <i class='bx bx-plus' id ="${product.id}" ></i>
-                        </div>      
+        html += ` 
+                
+                <div class="card">
 
+                    <div class="card_top">
+                        <img class="img_producto" src="${product.image}" alt="imagen">
                     </div>
-                    
-                `;
-        }
-                productsHTML.innerHTML = html;
-    }
 
-    console.log(main());
+                    <div class="card_dawn">
+                        <span>$${product.price}&nbsp;&nbsp;&nbsp;</span>
+                        <span> Stock: ${product.quantity}</span><br>
+                        <p class = "description_product">${product.name}</p>
+                        <div class = "add">
+                        <i class='bx bx-plus' id ="${product.id}" ></i>
+                        </div>
+                    </div>      
+
+                </div>
+                
+            `;
+    }    
+    productsHTML.innerHTML = html;
 }
 
-//* Nav Transparente
-{
-    window.onscroll = function() {
-        if (document.body.scrollTop > window.innerHeight) {
-            document.querySelector('.nav').classList.add('nav_show');
-        } else {
-            document.querySelector('.nav').classList.remove('nav_show');
-        }
+function mostrarCart() {
+    const cartIconHTML =document.querySelector('.cart_icon');
+    const cartHTML =document.querySelector('.cart');
+
+    cartIconHTML.addEventListener('click',function(){
+        cartHTML.classList.add('cart_show');
+    });
+
+    const hiddenCartHTML = document.querySelector('.cart_cerrar')
+
+    hiddenCartHTML.addEventListener('click',function(){
+        cartHTML.classList.remove('cart_show');
+    })
+}
+
+function insertarProductosAlCart(db) {
+    const cardProducts = document.querySelector('.card_products');
+
+    let html = '';
+
+    for(const product in db.cart){
+        const {quantity, price, name, image, id, amount} = db.cart[product];
+        html += `
+            <div class = "card_product">
+                <div class = "card_product--img">
+                    <img class = "img_cart_in"src="${image}" alt = "image" />
+                </div>
+                <div class = "card_product_body">
+                    <h4>${name} | $${price}</h4>
+                    <p>Stock: | ${quantity}</p>
+
+                    <div class = "card_product_ body_op">
+                        <i class='bx bx-minus'></i>
+                        <span>${amount} unit </span>
+                        <i class='bx bx-plus' ></i>                        
+                        <i class='bx bxs-trash' ></i>
+                    </div>
+
+                </div>
+                
+            </div>
+        `
+        
     }
+    console.log(html)
+    cardProducts.innerHTML = html;
+}
+
+function validarDatosCart(db){
+
+        const productHTML = document.querySelector('.products')
+
+        productHTML.addEventListener('click', function(e){
+            if(e.target.classList.contains('bx-plus')){
+                const id = Number(e.target.id);
+                
+                const productFind = db.products.find(
+                    (product) => product.id === id
+                );
+        
+                if(db.cart[productFind.id]){
+                    if(productFind.quantity === db.cart[productFind.id].amount) return alert ('No disponemos de mas en bodega');
+                    db.cart[productFind.id].amount++;
+                }else{
+                    db.cart[productFind.id] = {...productFind, amount: 1};
+                }
+        
+                window.localStorage.setItem('cart', JSON.stringify(db.cart))                               
+            }
+        
+        });
+
+}
+
+
+
     
-}
